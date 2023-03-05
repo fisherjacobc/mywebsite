9bdc967b8d2ae1b0e4b576ebb9b629e7c4c08da5
fisherjacobc <72705185+F1sxher@users.noreply.github.com>
2023-03-05 12:34:00 -0500

    complete i think?

---
+++ src/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+    return (
+      <>
+        <title>codiium</title>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta name="description" content="Experienced Developer" />
+        <link rel="icon" href="/favicon.ico" />
+      </>
+    )
+  }
